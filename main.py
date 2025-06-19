30ed1020d8f2a2c155eba2d20a02359eda68a8d0
eddiemosh <hardyedward18@gmail.com>
2025-06-20 00:28:50 +0100

    add docker file

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import random
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, world!"}
+top_uk_lagers = [
+    "Stella Artois",
+    "Budweiser",
+    "Foster’s",
+    "Carling",
+    "San Miguel",
+    "Carlsberg",
+    "Heineken",
+    "Peroni Nastro Azzurro",
+    "Corona",
+    "Desperados",
+    "Kronenbourg 1664",
+    "Beck’s",
+    "Staropramen",
+    "John Smith’s",
+    "Tennent’s Lager",
+    "Asahi Super Dry",
+    "Tyskie",
+    "Birra Moretti",
+    "Madri Excepcional",
+    "Coors"
+]
+@app.get("/", tags=["Beer"])
+def get_beer():
+    return {"beer": top_uk_lagers[random.randint(0,len(top_uk_lagers)-1)]}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "query": q}
