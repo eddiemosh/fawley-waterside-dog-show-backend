c1c0de9ee96f4c2a46d0205b6c1bfca9e398e014
eddiemosh <hardyedward18@gmail.com>
2025-06-20 01:13:09 +0100

    smaller beer

--- main.py
+++ main.py
@@ -30,8 +30,9 @@ top_uk_lagers = [
 
 @app.get("/", tags=["Beer"],
          responses={
-             status.HTTP_200_OK: {"description": "i love beer! <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Beer_mug.svg/1200px-Beer_mug.svg.png' alt='beer'>"}
-         }
+             status.HTTP_200_OK: {
+                 "description": "I love beer! <br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Beer_mug.svg/1200px-Beer_mug.svg.png' alt='beer' style='width:50%;'/>"
+             }
          )
 def get_beer():
     return {"beer": top_uk_lagers[random.randint(0, len(top_uk_lagers) - 1)]}
