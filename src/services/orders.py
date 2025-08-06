a188d3e6a57f90539eb7b1a88b5ddb0a1f267014
eddiemosh <hardyedward18@gmail.com>
2025-08-07 00:37:00 +0100

    logs

--- src/services/orders.py
+++ src/services/orders.py
@@ -29,7 +29,11 @@ class OrderService:
         try:
             order_id = str(uuid.uuid4())
             list_of_dogs = []
+            print("Doggie info is", doggie_info)
+            print(f"Doggie info type is {type(doggie_info)}")
             for info in doggie_info:
+                print("info is", info)
+                print(f"info type is {type(info)}")
                 list_of_dogs.append(DoggieInfo(**info))
 
             order = Order(
