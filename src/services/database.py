2d8c4c31d001f7a8ba70f92be1c6e8191426faa2
eddiemosh <hardyedward18@gmail.com>
2025-08-06 20:34:52 +0100

    orders stuff

--- src/services/database.py
+++ src/services/database.py
@@ -1,7 +1,7 @@
 import os
 from urllib.parse import quote_plus
 
-import boto3
+import json
 from pymongo import MongoClient
 
 from src.services.orders import Order
@@ -13,7 +13,8 @@ class Database:
     """
 
     def __init__(self):
-        db_password = os.environ.get("DB_PASSWORD")
+        db_credentials = json.loads(os.environ.get("DB_CREDENTIALS"))
+        db_password = db_credentials.get("password")
         print(f"password shortened is type {type(db_password)}")
         print(f"and content {db_password}")
         connection_string = f"mongodb://dogshow:{quote_plus(db_password)}@dogshow.cluster-c3owqu6m8ncl.eu-north-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
