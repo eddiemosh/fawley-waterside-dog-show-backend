efa92a86f11f91b6621a4a5b75a00b88fc45a0a8
eddiemosh <hardyedward18@gmail.com>
2025-08-09 02:10:00 +0100

    reformat

--- src/routers/test_router.py
+++ src/routers/test_router.py
@@ -18,3 +18,12 @@ def toggle_test_mode(test_mode: bool):
         return {"message": f"Test mode set to {test_mode}"}
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
+
+@router.get("/", status_code=HTTPStatus.OK)
+def get_test_mode():
+    try:
+        order_service = OrderService()
+        test_mode = order_service.get_test_mode()
+        return {"test_mode": test_mode}
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
