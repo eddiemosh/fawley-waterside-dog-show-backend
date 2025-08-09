ec3d9e6bc7a5f56b18dfc1dcc04c1d781f9380e5
eddiemosh <hardyedward18@gmail.com>
2025-08-09 17:23:34 +0100

    delete test data

--- src/services/database.py
+++ src/services/database.py
@@ -32,24 +32,7 @@ class Database:
         self.test_collection = db["test"]
         self.orders_collection.create_index("order_id", unique=True)
         self._initialized = True
-
-        count = 0
-        orders = self.get_orders()
-        for order in orders:
-            if (order.get("first_name") == "Edward" or order.get("first_name") == "Ian") and order.get(
-                "last_name"
-            ) == "Hardy":
-                self.delete_order(order_id=order.get("order_id"))
-                count += 1
-
-            if (
-                order.get("email_address") == "ian.hardy2106@icloud.com"
-                or order.get("email_address") == "bloogy.doggy@iknow.com"
-            ):
-                self.delete_order(order_id=order.get("order_id"))
-                count += 1
-
-        print(f"Deleted {count} orders")
+        self.delete_order(order_id="NFLVVS")
 
     def get_order(self, order_id: str) -> dict:
         result = self.orders_collection.find_one({"order_id": order_id}, {"id": 0})
