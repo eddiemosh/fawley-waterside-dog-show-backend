3374ff7a14f1756213842c9153becda9be0be83f
eddiemosh <hardyedward18@gmail.com>
2025-08-11 20:44:12 +0100

    some feedback work

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from src.routers import analytics_router, order_router, payment_router, test_router
+from src.routers import analytics_router, order_router, payment_router, test_router, feedback_router
 
 app = FastAPI(title="Dogshow Backend")
 
@@ -9,6 +9,7 @@ app.include_router(payment_router.router)
 app.include_router(order_router.router)
 app.include_router(analytics_router.router)
 app.include_router(test_router.router)
+app.include_router(feedback_router.router)
 
 origins = [
     "http://localhost:3000",  # Local frontend (React dev server)
